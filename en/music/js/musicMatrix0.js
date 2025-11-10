aa3499a21501a4058e67ce8112eb4134a242dbe5
Valerii-69 <119952076+Valerii-69@users.noreply.github.com>
2025-11-11 07:44:43 +1100

    Add files via upload

---
+++ en/music/js/musicMatrix0.js
@@ -0,0 +1,18 @@
+window.modulesMusic1 = [
+{ title: "Wave of Silence", content: "chapters/wave_silence_en.html", model: "wave" },
+{ title: "Path of Fire - Wave Saw", content: "chapters/fire_path_en.html", model: "fire" },
+{ title: "Path of Water - Smooth Transitions", content: "chapters/path-of-water-en.html", model: "wave" },
+{ title: "Path of Earth - Steady Rhythms", content: "chapters/path-earth-en.html", model: "crystal" },
+{ title: "Path of Ether - Harmonic Shadows", content: "chapters/ether_path_en.html", model: "wave" },
+{ title: "Maiden Dreams - Three Synthesizers", content: "chapters/dreams_en.html", model: "fire" },
+{ title: "Musical Spiral", content: "chapters/spiral_en.html", model: "crystal" },
+{ title: "Rhythms of the Planets", content: "chapters/planetary_en.html", model: "wave" },
+{ title: "Voices of the Spheres", content: "chapters/voices_en.html", model: "wave" },
+{ title: "Numbers in Melody", content: "chapters/numbers_en.html", model: "crystal" },
+{ title: "The Mathematics of Consonance", content: "chapters/consonance_en.html", model: "fire" },
+{ title: "Music and Fractals", content: "chapters/fractals_en.html", model: "wave" },
+{ title: "Memory Waves", content: "chapters/memory_en.html", model: "wave" },
+{ title: "Subtle Rhythms", content: "chapters/subtle_en.html", model: "crystal" },
+{ title: "Chaos and Melody", content: "chapters/chaos_en.html", model: "fire" },
+{ title: "Music Laboratory", content: "lab/index_03.html", model: "crystal" }
+];
